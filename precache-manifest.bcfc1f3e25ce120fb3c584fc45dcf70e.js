700c89cded184419a9a48f3a5f4147188c25148f
Alina Ray <a.alina.ray@gmail.com>
2019-07-04 23:33:26 +0300

    Updates

---
+++ precache-manifest.bcfc1f3e25ce120fb3c584fc45dcf70e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbbd1e233a2a6d0102e0c1d12690ebad",
+    "url": "/fe_on_feb19-react-dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "2519685a9548e5f54031",
+    "url": "/fe_on_feb19-react-dynamic-list-of-posts/static/css/main.956f3944.chunk.css"
+  },
+  {
+    "revision": "0f8d382090bb5b80b8eb",
+    "url": "/fe_on_feb19-react-dynamic-list-of-posts/static/js/2.fd0c881e.chunk.js"
+  },
+  {
+    "revision": "2519685a9548e5f54031",
+    "url": "/fe_on_feb19-react-dynamic-list-of-posts/static/js/main.8bc70d3b.chunk.js"
+  },
+  {
+    "revision": "f16cba658a74199d659c",
+    "url": "/fe_on_feb19-react-dynamic-list-of-posts/static/js/runtime~main.259f560b.js"
+  }
+]);
